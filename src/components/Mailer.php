b11d0e4c1467f32932d775280c560bffe468f545
demirmehmet0 <demirmehmet0@yahoo.com>
2023-12-15 14:43:22 +0300

    feat: add mailer

--- src/components/Mailer.php
+++ src/components/Mailer.php
@@ -4,6 +4,7 @@ namespace portalium\site\components;
 
 use Yii;
 use portalium\components\Mailer as CoreMailer;
+use Symfony\Component\Mailer\Transport\Smtp\SmtpTransport;
 
 class Mailer extends CoreMailer
 {
@@ -12,12 +13,13 @@ class Mailer extends CoreMailer
         parent::init();
 
         $transport = [
-            'class' => 'Swift_SmtpTransport',
+            'class' => SmtpTransport::class,
             'host' => Yii::$app->setting->getValue('smtp::server'),
             'username' => Yii::$app->setting->getValue('smtp::username'),
             'password' => Yii::$app->setting->getValue('smtp::password'),
             'port' => Yii::$app->setting->getValue('smtp::port'),
             'encryption' => Yii::$app->setting->getValue('smtp::encryption'),
+            'scheme' => 'smtp'
         ];
 
         $this->setTransport($transport);
